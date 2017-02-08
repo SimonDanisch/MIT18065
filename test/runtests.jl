ee77ab60cade109219a5c2bb9271dcd7d14c8b72
SimonDanisch <sdanisch@gmail.com>
2017-02-08 16:58:26 -0500

    update install

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,24 @@
 using IJulia
-notebook(dir = joinpath(dirname(@__FILE__), "..", "docs"))
+
+# some checks:
+
+if is_linux()
+    needsrebuild = false
+    if !success(`cmake --version`)
+        info("Please execute sudo apt-get install cmake in your terminal")
+        needsrebuild = true
+    end
+    if !contains(readstring(`dpkg-query -W -f='${Status}' xorg-dev`), "ok")
+        info("Please execute `sudo apt-get install xorg-dev` in your terminal")
+        needsrebuild = true
+    end
+    if !contains(readstring(`dpkg-query -W -f='${Status}' libglu1-mesa-dev`), "ok")
+        info("Please execute `sudo apt-get install libglu1-mesa-dev` in your terminal")
+        needsrebuild = true
+    end
+    if needsrebuild
+        info("Please run Pkg.build(\"MIT18065\")")
+    end
+end
+
+notebook(detach = true, dir = joinpath(dirname(@__FILE__), "..", "docs"))
